d472e97c944024cdd7574824379d4af38ac539e3
Der324 <ntalenderick@gmail.com>
2025-10-06 04:43:07 +0300

    bulding sentence maker

---
+++ Sentence Maker/script.js
@@ -0,0 +1,32 @@
+let adjective;
+adjective = "brave"
+
+let noun;
+noun = "dragon";
+
+let verb;
+verb = "angry";
+
+let place;
+place = "castle"
+
+let adjective2;
+adjective2 = "fiery"
+
+let noun2;
+noun2 = "marshmallows";
+
+let firstStory;
+firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log(`First story: ${firstStory}`);
+
+adjective = "tiny";
+noun = "unicorn";
+verb = "happy";
+place = "forest";
+adjective2 = " sparly";
+noun2 = "cupcakes";
+
+let secondStory;
+secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log(`Second story: ${secondStory}`);
