708157d3a03ba98396effe2767a9ca13b1aae959
Der324 <ntalenderick@gmail.com>
2026-04-09 13:09:19 +0200

    Boolean Check Function

---
+++ JAVASCRIPT/Boolean-Check-Function.js
@@ -0,0 +1,11 @@
+/*Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
+
+User Stories:
+
+You should have a function called booWho that receives one argument.
+If the argument received is a boolean primitive, the function should return true.
+If the argument is any other value, the function should return false.*/
+
+function booWho(value){
+  return typeof value === "boolean";
+}
